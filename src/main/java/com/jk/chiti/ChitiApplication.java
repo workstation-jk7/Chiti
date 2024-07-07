e67b03ba8017fe954d35aa9ae99f49e0b662421b
Karthik <workstation.jk7@gmail.com>
2024-07-08 03:30:28 +0530

    Chiti Application class and spring boot & maven config

---
+++ src/main/java/com/jk/chiti/ChitiApplication.java
@@ -0,0 +1,15 @@
+package com.jk.chiti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class ChitiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChitiApplication.class, args);
+		System.out.println("Jai Shri Ram. Jai jai hanuman. Jai ShivaShankara");
+	}
+
+}
---
+++ src/test/java/com/jk/chiti/ChitiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jk.chiti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChitiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
